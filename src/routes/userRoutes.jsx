b091150f4bcd26d7edfdddbc535bb796e228cfb6
Izuuii <denverdeleon22@gmail.com>
2025-07-21 21:27:19 +0800

    User/Student Routes

--- src/routes/userRoutes.jsx
+++ src/routes/userRoutes.jsx
@@ -1,15 +1,23 @@
+// src/routes/UserRoutes.jsx
+import React from "react";
 import { Route } from "react-router-dom";
 import UserLayout from "../layouts/userLayout";
 import UserDashboard from "../pages/user/Dashboard";
 import Grade from "../pages/user/Grade";
+import UserAttendance from "../pages/user/UserAttendance";
+import HealthProfile from "../pages/user/HealthProfile";
+import TextBooks from "../pages/user/TextBooks";
+import Achievements from "../pages/user/Achievements";
 
-const UserRoutes = [
-  <>
-    <Route element={<UserLayout />}>
-      <Route path="/student/dashboard" element={<UserDashboard />} />
-      <Route path="/grade" element={<Grade />} />
-    </Route>
-  </>,
-];
+const UserRoutes = (
+  <Route element={<UserLayout />}>
+    <Route path="/student/dashboard" element={<UserDashboard />} />
+    <Route path="/grade" element={<Grade />} />
+    <Route path="/userAttendance" element={<UserAttendance />} />
+    <Route path="/health-profile" element={<HealthProfile />} />
+    <Route path="/text-books" element={<TextBooks />} />
+    <Route path="/achievements" element={<Achievements />} />
+  </Route>
+);
 
 export default UserRoutes;
