be6627f8a26b86d142cbf9d7738dc51059277a3a
Izuuii <denverdeleon22@gmail.com>
2025-07-21 21:17:13 +0800

    Student Grade Table

--- src/routes/userRoutes.jsx
+++ src/routes/userRoutes.jsx
@@ -1,11 +1,13 @@
 import { Route } from "react-router-dom";
 import UserLayout from "../layouts/userLayout";
 import UserDashboard from "../pages/user/Dashboard";
+import Grade from "../pages/user/Grade";
 
 const UserRoutes = [
   <>
     <Route element={<UserLayout />}>
       <Route path="/student/dashboard" element={<UserDashboard />} />
+      <Route path="/grade" element={<Grade />} />
     </Route>
   </>,
 ];
