d95a6e4586bf1ac78494a0ec6740ab2d3260078c
Renz-Eryll <renzeryll09@gmail.com>
2025-10-21 14:43:05 +0800

    fix: protected routes

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,20 +1,22 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuthStore } from "../stores/auth";
-import LoadingPage from "../components/common/LoadingPage";
 
-const ProtectedRoute = ({ allowedRoles, children }) => {
+const ProtectedRoute = ({ allowedRoles = [], children }) => {
   const { user, isCheckingAuth } = useAuthStore();
   const location = useLocation();
 
+  // Show loading while checking auth
   if (isCheckingAuth) {
-    return;
+    return <div>Loading...</div>;
   }
 
+  // Redirect to login if not authenticated
   if (!user) {
     return <Navigate to="/sign-in" state={{ from: location }} replace />;
   }
 
-  if (!allowedRoles.includes(user.role)) {
+  // Redirect if user role not in allowed roles
+  if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
     return <Navigate to={`/${user.role}/dashboard`} replace />;
   }
 
